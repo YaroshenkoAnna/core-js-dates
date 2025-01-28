41362bbf8e0913b78680c03ad0e57b3593537e38
Anna <yaroshenkoannette@gmail.com>
2025-01-28 16:25:02 +0100

    add soli

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -70,8 +70,13 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const eventDate = new Date(date);
+  const currentDay = eventDate.getDay();
+  const daysUntilFriday = (5 - currentDay + 7) % 7 || 7;
+  eventDate.setDate(eventDate.getDate() + daysUntilFriday);
+
+  return eventDate;
 }
 
 /**
