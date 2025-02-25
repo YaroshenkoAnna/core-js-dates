b8abccaf0ac506538b244b553552bf84a6904cbd
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2025-02-25 20:23:11 +0100

    feat: add implementation of getCountWeekendsInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -178,8 +178,16 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let countWeekends = 0;
+  const daysInMonth = getCountDaysInMonth(month, year);
+  for (let i = 1; i <= daysInMonth; i += 1) {
+    const date = new Date(year, month - 1, i);
+    if (date.getDay() === 0 || date.getDay() === 6) {
+      countWeekends += 1;
+    }
+  }
+  return countWeekends;
 }
 
 /**
