baddc5bbce925e8a00206301955d540bb9837bf3
Anna Yaroshenko <yaroshenkoannette@gmail.com>
2025-02-25 20:51:57 +0100

    feat: add implementation of getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -226,8 +226,14 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const eventDate = new Date(date);
+  eventDate.setDate(13);
+  while (eventDate.getDay() !== 5) {
+    eventDate.setMonth(eventDate.getMonth() + 1);
+    eventDate.setDate(13);
+  }
+  return eventDate;
 }
 
 /**
